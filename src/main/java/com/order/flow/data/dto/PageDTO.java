0459361234a555b41d622ad3c904047924457515
DongHoKim <cha11enge@hanmail.net>
2023-08-11 11:30:04 +0900

    [mod] page Default Value 변경

--- src/main/java/com/order/flow/data/dto/PageDTO.java
+++ src/main/java/com/order/flow/data/dto/PageDTO.java
@@ -6,10 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PageDTO {
-  private int page = 1;
+  private int page = 0;
   private int size = 10;
 
   public void setPage(int page) {
-    this.page = page - 1;
+    this.page = 0 < page? page - 1: 0;
   }
 }
